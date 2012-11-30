7ba7ec42582854fc27c133f336a99bd221b0dcf9
Oshane Bailey <b4_oshany@yahoo.com>
2012-11-29 19:31:43 -0500

    try out

--- etc/create_config.php
+++ etc/create_config.php
@@ -1,27 +1,58 @@
 <?php
-function createConnection($mysql_user, $mysql_pass, $mysql_db, $db){
+function createConnectionFile($mysql_user, $mysql_pass, $mysql_db, $db){
 	if($db){
 			file_put_contents('../etc/connect.inc.php',
 '<?php
-	//privilige user for the database
-	$mysql_user = \''.$mysql_user.'\';
-	$mysql_pass = \''.$mysql_pass.'\';
 	
-	//set the database to use
-	$mysql_db = \''.$mysql_db.'\'; 
+	abstract class DatabaseConnect{		
+		//privilige user for the database
+		private $mysql_user = \''.$mysql_user.'\';
+		private $mysql_pass = \''.$mysql_pass.'\';
 	
-	try{
-	# connect to the database
-		$db = new PDO("mysql:dbname=".$mysql_db."", "".$mysql_user."", "".$mysql_pass.""); //create a new pdo object
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //setting error handling
-	}catch (PDOException $e){
-		echo \'Errrr!: \'.$e->getMessage();	
+		//set the database to use
+		private $mysql_db = \''.$mysql_db.'\'; 
+		
+		const errr = \'<br> An sql error has occured\';
+		
+		final public function __construct(){
+			echo \'Attempting connection.........\';
+			if($this->connect() !=true){
+				echo \'connection failed\';
+			}else{
+				echo \'connected\';
+			}
+		}
+		
+		final private function connect(){
+			try{
+				# connect to the database
+				$db = new PDO("mysql:dbname=".$this->mysql_db, $this->mysql_user, $this->mysql_pass);
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}catch (Exception $e){
+				echo \'Errrr!: \'.$e->getMessage();	
+			}
+		}
 	}
 ?>', 
 		LOCK_EX );
 	}else{	
 		echo 'could not connect';
 	}
+}
+function createProfileFile($sname, $cname, $slogan){
+	file_put_contents('../etc/profile.inc.php',
+'<?php
+	//name of the site
+	$sname = \''.$sname.'\';
+	
+	//campany name
+	$cname = \''.$cname.'\';
+	
+	//slogo of the company
+	$slogan = \''.$slogan.'\'; 
+?>', 
+	LOCK_EX );
+
 
 }
 ?>
