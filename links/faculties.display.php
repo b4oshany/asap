357b32f00bc50cd8c21a42beec87852888844b07
Oshane Bailey <b4_oshany@yahoo.com>
2012-12-08 18:45:11 -0500

    adjust the data visualization class

--- links/faculties.display.php
+++ links/faculties.display.php
@@ -7,8 +7,12 @@ $facVis = new DataVisualization;
 $humanities = new Faculty;
 $sqlResults = $humanities->SelectQuery($humanities->select, $humanities->from, $humanities->condition); 
 $display = $humanities->setSqlOut($sqlResults);
+
 $array = $humanities->setArrayDataOut($humanities->ReturnArrayData($display));
+echo '<br>';
+print_r($array);
 $hdata = $humanities->SingleFacultyGraph();
+echo '<br>'.$hdata.'<br>';
 $facVis->BarGraph($hdata)
 	?>
 <div id="chart_div" style="width: 900px; height: 500px;"></div>
